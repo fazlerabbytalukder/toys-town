5c77acada08ad77205b7a99be7f44b0a9964f30f
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-11-04 08:53:41 -0700

    data

--- src/context/productcontext.jsx
+++ src/context/productcontext.jsx
@@ -1,8 +1,23 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext();
 
+const API = "https://raw.githubusercontent.com/fazlerabbytalukder/toys-town/main/public/Data.json";
+
 const AppProvider = ({ children }) => {
+    const getProducts = async (url) => {
+        const res = await axios.get(url);
+        const products = await res.data;
+        console.log(products);
+    }
+
+
+    useEffect(() => {
+        getProducts(API);
+    }, []);
+
+
     return <AppContext.Provider value={{myName: "Rabby"}}>
         {children}
     </AppContext.Provider>
