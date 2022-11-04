95e49d31ea92bd532294c15d7c8c0404c03ccd2b
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-11-03 19:22:15 -0700

    projects setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+import Cart from './components/Cart/Cart';
+import Contacts from './components/Contact/Contacts';
+import Error from './components/Error/Error';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import SingleProduct from './components/SingleProduct/SingleProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/contact' element={<Contacts/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/singleproduct/:id' element={<SingleProduct />} />
+        <Route path='*' element={<Error/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
